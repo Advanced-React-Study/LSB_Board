63590b560f546e8ec29922fc87e18d678e00745e
Subin <supiao0123@gmail.com>
2022-10-10 17:13:14 +0900

    FEAT: add login and register page

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Input from "../../components/Input";
-import LoginWrapper from "./LoginWrapper";
+import LoginWrapper from "../login/LoginWrapper";
 import InputWrapper from "../../components/InputWrapper";
 import Button from "../../components/Button";
 import axios from "axios";
 
-const Login = () => {
+const Register = () => {
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
 
@@ -16,17 +16,17 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const onClickLogin = () => {
-    axios.post("http://43.201.8.111:3001/user/login", {
+  const onClickRegister = () => {
+    axios.post("http://43.201.8.111:3001/user/register", {
       id: userId,
       password: password,
     });
-    alert("Login successfully");
+    alert("Register successfully");
   };
 
   return (
     <LoginWrapper>
-      <h1>로그인</h1>
+      <h1>회원 가입</h1>
       <InputWrapper>
         <Input
           type="text"
@@ -42,10 +42,9 @@ const Login = () => {
           placeholder={"PW:"}
         />
       </InputWrapper>
-      <Button onClick={onClickLogin}>login</Button>
-      <a href="/register">회원 가입</a>
+      <Button onClick={onClickRegister}>register</Button>
     </LoginWrapper>
   );
 };
 
-export default Login;
+export default Register;
